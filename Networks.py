01499df48ba5b0ddf79287ec158d8ad701e31eac
tgexterkate <119415750+tgexterkate@users.noreply.github.com>
2023-06-24 12:37:32 +0200

    added networks.py and added training loop to dataset_class

---
+++ Networks.py
@@ -0,0 +1,179 @@
+import torch.nn as nn
+from torchvision.ops import MLP
+
+class ConvWB(nn.Module):
+    """
+    ConvWB network
+    """
+
+    def __init__(self, in_channels=3, hidden_channels=[16,32,128]):
+        """
+        Initialize the ConvWB network.
+        Input: one or multiple images
+        Returns: Input images with enhanced whitebalance obtained through
+        multiplication of Input image with output of last network layer
+        """
+        super(ConvWB, self).__init__()
+
+
+        self.conv1 = nn.Conv2d(in_channels, hidden_channels[0],
+                                kernel_size=7,
+                                padding=3)
+        self.leakyrelu1 = nn.LeakyReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=4)
+        self.conv2 = nn.Conv2d(hidden_channels[0], hidden_channels[1],
+                                kernel_size=5,
+                                padding=2)
+        self.leakyrelu2 = nn.LeakyReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3)
+        self.conv3 = nn.Conv2d(hidden_channels[1], hidden_channels[2],
+                                kernel_size=3,
+                                padding=1)
+        self.leakyrelu3 = nn.LeakyReLU()
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+        self.adaptpool = nn.AdaptiveAvgPool2d(1)
+        self.mlp = MLP(in_channels = 128, hidden_channels = [3])
+
+
+
+    def forward(self, input_image):
+
+
+        x = self.conv1(input_image)
+        x = self.leakyrelu1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.leakyrelu2(x)
+        x = self.maxpool2(x)
+        x = self.conv3(x)
+        x = self.leakyrelu3(x)
+        x = self.maxpool3(x)
+        x = self.adaptpool(x)
+        x = x.reshape(x.size(0),-1)
+
+        output = self.mlp(x)
+
+        ######### debugging ########
+        # output[0][0] = 0.8
+        # output[0][1] = 0.8
+        # output[0][2] = 0.8
+        ############################
+
+
+
+        print('WB MATRIX VALUES ARE:', output[0])
+
+        enhanced_image = input_image.clone()
+        for i in range(len(input_image)):
+          enhanced_image[i, 0, :, :] = input_image[i, 0, :, :] * output[i, 0]
+          enhanced_image[i, 1, :, :] = input_image[i, 1, :, :] * output[i, 1]
+          enhanced_image[i, 2, :, :] = input_image[i, 2, :, :] * output[i, 2]
+
+
+        return enhanced_image
+
+class ConvCC(nn.Module):
+    """
+   ConvCC network
+    """
+
+    def __init__(self, in_channels=3, hidden_channels=[16,32,128]):
+        """
+        Initialize the ConvCC network.
+        Input: The enhanced images of the ConvWB network.
+        Returns: Image with a color space that is optimal to input
+        into a shallow ConvNet (next network).
+        """
+        super(ConvCC, self).__init__()
+
+
+        self.conv1 = nn.Conv2d(in_channels, hidden_channels[0],
+                                kernel_size=7,
+                                padding=3)
+        self.leakyrelu1 = nn.LeakyReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=4)
+        self.conv2 = nn.Conv2d(hidden_channels[0], hidden_channels[1],
+                                kernel_size=5,
+                                padding=2)
+        self.leakyrelu2 = nn.LeakyReLU()
+        self.maxpool2 = nn.MaxPool2d(kernel_size=3)
+        self.conv3 = nn.Conv2d(hidden_channels[1], hidden_channels[2],
+                                kernel_size=3,
+                                padding=1)
+        self.leakyrelu3 = nn.LeakyReLU()
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2)
+        self.adaptpool = nn.AdaptiveAvgPool2d(1)
+
+        self.mlp = MLP(in_channels = 128, hidden_channels = [9])
+
+
+
+    def forward(self, input_image):
+
+
+        x = self.conv1(input_image)
+        x = self.leakyrelu1(x)
+        x = self.maxpool1(x)
+        x = self.conv2(x)
+        x = self.leakyrelu2(x)
+        x = self.maxpool2(x)
+        x = self.conv3(x)
+        x = self.leakyrelu3(x)
+        x = self.maxpool3(x)
+        x = self.adaptpool(x)
+        #x = x.permute(0, 2, 3, 1)
+        x = x.reshape(x.size(0),-1)
+        output = self.mlp(x)
+
+        print('CC MATRIX VALUES ARE:', output[0])
+
+
+        enhanced_image = input_image.clone()
+        for i in range(len(input_image)):
+          enhanced_image[i,0,:,:] = input_image[i,0,:,:]*output[i,0] + input_image[i,1,:,:]*output[i,1] + input_image[i,2,:,:]*output[i,2]
+          enhanced_image[i,1,:,:] = input_image[i,0,:,:]*output[i,3] + input_image[i,1,:,:]*output[i,4] + input_image[i,2,:,:]*output[i,5]
+          enhanced_image[i,2,:,:] = input_image[i,0,:,:]*output[i,6] + input_image[i,1,:,:]*output[i,7] + input_image[i,2,:,:]*output[i,8]
+
+
+        return enhanced_image
+
+class ShallowConv(nn.Module):
+    """
+    Shallow Convolutional network
+    """
+
+    def __init__(self, in_channels=3, hidden_channels=[16,64,3]):
+        """
+
+        """
+        super(ShallowConv, self).__init__()
+
+
+        self.conv1 = nn.Conv2d(in_channels, hidden_channels[0],
+                                kernel_size=3,
+                                padding=1)
+        self.instancenorm1 = nn.InstanceNorm2d(16)
+        self.leakyrelu1 = nn.LeakyReLU()
+        self.conv2 = nn.Conv2d(hidden_channels[0], hidden_channels[1],
+                                kernel_size=3,
+                                padding=1)
+        self.instancenorm2 = nn.InstanceNorm2d(64)
+        self.leakyrelu2 = nn.LeakyReLU()
+        self.conv3 = nn.Conv2d(hidden_channels[1], hidden_channels[2],
+                                kernel_size=1,
+                                padding=0)
+
+    def forward(self, input_image):
+
+
+        x = self.conv1(input_image)
+        x = self.instancenorm1(x)
+        x = self.leakyrelu1(x)
+        x = self.conv2(x)
+        x = self.instancenorm2(x)
+        x = self.leakyrelu2(x)
+        x = self.conv3(x)
+
+
+
+        return x
--- dataset_class.py
+++ dataset_class.py
@@ -3,21 +3,19 @@ import rawpy
 print('')
 import json
 import numpy as np
-import torch.nn as nn
 from matplotlib import pyplot as plt
 import cv2
-from torchvision.ops import MLP
 from torch.utils.data import Dataset, DataLoader
-# import pyrawide
+import torchvision.models as models
+from Networks import ConvCC, ConvWB, ShallowConv
+import torch.optim as optim
 
 
 
 import os
 import torch
-import torch.nn as nn
 from torchsummary import summary
 import torch.nn.functional as F
-from torchvision.ops import MLP
 
 def preprocessing(path):
     raw = rawpy.imread(path)
@@ -121,11 +119,112 @@ for item in annots_list:
 print(image_annots)
 
 
+
+
+
+# define the model
+# of the shelf retinanet with resnet50
+retinanet = models.detection.retinanet_resnet50_fpn_v2(weights='DEFAULT')
+retinanet = retinanet.cuda()
+
+# Freeze the parameters of the backbone
+for param in retinanet.parameters():
+    param.requires_grad = False
+
+# our own model
+convwb_model = ConvWB()
+convwb_model.to(torch.double)
+convcc_model = ConvCC()
+convcc_model.to(torch.double)
+shallowconv_model = ShallowConv()
+shallowconv_model.to(torch.double)
+
+# Get the parameters from each model
+convwb_params = list(convwb_model.parameters())
+convcc_params = list(convcc_model.parameters())
+shallowconv_params = list(shallowconv_model.parameters())
+
+# Combine the parameters into a single list
+all_params = convwb_params + convcc_params + shallowconv_params
+
+# use adam optimazation
+optimizer = optim.Adam(all_params, lr=0.01)
+
+
+# Learning rate schedule
+# learning_rate = [0.01, 0.001, 0.0001] at 5th and 10th epoch change
+lr_schedule = optim.lr_scheduler.MultiStepLR(optimizer, milestones=[5, 10], gamma=0.1)
+
+# See if GPU is available + move stuff
+# mini_batch moet nog naar cuda en de optimizers ook
+if torch.cuda.is_available():
+    print('naar cuda gezet')
+    convwb_model.cuda()
+    convcc_model.cuda()
+    shallowconv_model.cuda()
+else:
+    pass
+
+convwb_model.train()
+convcc_model.train()
+shallowconv_model.train()
+
 dataset = CustomDataset(image_paths,image_annots)
 # print(dataset)
 
-dataloader = DataLoader(dataset, batch_size=2)
-for batch_images in dataloader:
-    print(batch_images.shape)
-    # print(batch_annotations)
-    print('training.............................................')
+dataloader = DataLoader(dataset, batch_size=5)
+
+# for batch_images in dataloader:
+#     print(batch_images.shape)
+#     # # print(batch_annotations)
+#     print('training.............................................')
+
+epochs = 15
+batchs = 8
+epoch_number = 0
+batch_number = 0
+
+# Training loop
+for i, epoch in enumerate(range(epochs)):
+    print('EPOCH {}:'.format(epoch_number + 1))
+
+    # Move the mini-batch data and targets to CUDA
+
+
+    
+
+    for batch_images in dataloader:    #batch in range(0, len(batch_images), batchs):
+        # print('BATCH {}:'.format(batch_number + 1))
+        batch_images_cuda = batch_images.cuda()
+        print(batch_images_cuda.shape)
+        #batch_targets_cuda = batch_targets.cuda()
+        # Zero the gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        output = convwb_model(batch_images_cuda)
+        print('output na convwv: ', output.shape)
+        output = convcc_model(output)
+        output = shallowconv_model(output)
+        output = retinanet(output)
+
+        ##!!!Delete later!!!##
+        # Print some stuff to check
+        print(output.size())
+        print(targets.size())
+
+        # Two losfunctions are used
+        # Regression loss = alpha balanced focal loss
+        # classification loss = smooth-L1 loss
+        L_reg = ops.focal_loss(output, targets, alpha=None, gamma=2.0, reduction='mean')
+        L_cls = nn.SmoothL1Loss(output, targets)
+        L_total = L_reg + L_cls
+        print(L_total)
+
+        # Backward pass
+        L_total.backward()
+
+        # Update the model parameters
+        optimizer.step()
+        # Adjust learning rate based on the schedule
+        lr_schedule.step()
